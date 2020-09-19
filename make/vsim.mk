6adfe5fb3389eefc08109d5e68c92457b5a2c5a6
Anand S <ansn@cypress.com>
2020-09-19 12:35:03 +0530

    Changing compile structure:
        Individual design directories in and of itself would be the run
        directory. Common things (part of simulator) are added in a new dir,
        and included at the design level. Would support multi lib compile
        when added to design level Makefile.REFLIBS var. Keeping top level
        Makefile for now. runs work even from there, but not advisable.
        TOPIMAGE is variablised. And a good hack was required to  not have
        additional too many files being produced as part of the flow - They
        are moved inside the work libraries created.

---
+++ make/vsim.mk
@@ -0,0 +1,58 @@
+export MODELVER=questasim_10.5c_5
+
+WORKLIBS     = $(REFLIBS:%=%$(WORKSFX))
+COMP_TARGETS = $(REFLIBS:%=$(WORKLIB)/%_compile)
+
+LSF = bsub -Ip
+VLIB= vlib
+VMAP= vmap
+VLOG= $(LSF) vlog
+VSIM= $(LSF) vsim -novopt
+VOPTFLAGS = +acc
+VOPT = $(LSF) vopt $(VOPTFLAGS)
+
+WORKSFX = _work
+
+%$(WORKSFX):
+	$(VLIB) $(@)
+	$(VMAP) $(@) $(@)
+
+# Below line tells make to 'ignore the timestamp of the dir'
+# https://stackoverflow.com/questions/1950926/create-directories-using-make-file
+# The pipe symbol tells make that the following prereqs are 'order-only'
+# 	                   	                                  |
+# 	                   +--------------------------------------+
+# 	                   |
+# 	                   v
+%$(WORKSFX)/$(WORKSFX).ts: | %$(WORKSFX)
+	touch $@
+
+.PHONY: setup
+setup: $(REFLIBS:%=%$(WORKSFX)/$(WORKSFX).ts)
+
+%$(WORKSFX)/_compile.ts: $(REFLIBS:%=%$(WORKSFX)/$(WORKSFX).ts)
+	$(VLOG) -work $(@D) $(WORKLIBS:%= -L %) -f $(@D:%$(WORKSFX)=%)/file_list
+	touch $@
+
+.PHONY: compile
+compile:$(REFLIBS:%=%$(WORKSFX)/_compile.ts)
+
+%$(WORKSFX)/_elab.ts: $(REFLIBS:%=%$(WORKSFX)/_compile.ts)
+	touch $@
+
+.PHONY: elab
+elab: $(REFLIBS:%=%$(WORKSFX)/_elab.ts)
+
+.PHONY: sim
+sim: $(REFLIBS:%=%$(WORKSFX)/_elab.ts)
+	$(VSIM) $(WORKLIBS:%= -L %) $(SIMOPTS) tb$(WORKSFX).top
+
+# Only for quick compile without library setup
+.PHONY: simclean
+simclean:
+	rm -rf *$(WORKSFX)/_elab.ts 
+	rm -rf *$(WORKSFX)/_compile.ts 
+
+.PHONY: simclobber
+simclobber: simclean
+	rm -rf *$(WORKSFX) modelsim.ini transcript 
