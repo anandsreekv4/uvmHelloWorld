05f1c81eb8d4f8b7b08c5e5e4b419354a7e38c41
Anand S <ansn@cypress.com>
2021-06-27 23:26:49 +0530

    Adding proper assertions for dbl syncs.
    The info was found from :
    https://www.techdesignforums.com/practice/technique/multiple-cross-clock-domain-verifcation/
    Check this out for more assertions for handshake and other scenarios as well

--- design/afifo_tb/dut/dbl_sync_asserts.sv
+++ design/afifo_tb/dut/dbl_sync_asserts.sv
@@ -28,8 +28,41 @@ module dbl_sync_asserts #(
    input [WIDTH-1:0] data_sync_o
 );
 
-   ERR_DBL_SYNC_DI_NOT_GRAY_ENC:
-     assert property (@(posedge clk_i) disable iff(!rstn_i || (WIDTH < 1))
-		      (!$stable(data_i)) |-> $onehot(data_i ^ $past(data_i)) && (WIDTH > 1));
-       					      
+    //------------------------------------------------------------------------
+    // Below property is encforcing gray encoding checks - NOT practical on 
+    // dbl syncs - Hence not asserting it - But useful for pure gray code based
+    // data migration across clock domains (Don't check inside dbl sync though)
+    //------------------------------------------------------------------------
+    property prop_dbl_sync_di_not_gray_enc;
+        @(posedge clk_i) disable iff(!rstn_i || (WIDTH < 1))
+        (!$stable(data_i)) |-> $onehot(data_i ^ $past(data_i)) && (WIDTH > 1));
+    endproperty: prop_dbl_sync_di_not_gray_enc;
+
+    //------------------------------------------------------------------------
+    // stability: checks that data_i is stable for two clocks (or 3 edges ??)
+    //------------------------------------------------------------------------
+    property stability;
+        @(posedge clk_i) disable iff(!rstn_i)
+            !$stable(data_i) |=> $stable(data_i)[*2];
+    endproperty: stability
+
+    //------------------------------------------------------------------------
+    // no_glitch: check for a glitch on data_i
+    //------------------------------------------------------------------------
+    property no_glitch;
+        logic [WIDTH-1:0] data;
+        @(data_i)
+            (1, data = !data_i) |=>
+                @(posedge clk_i)
+                    (data_i == data);
+    endproperty: no_glitch
+
+    //------------------------------------------------------------------------
+    // ASSERT PROPERTIES
+    //------------------------------------------------------------------------
+    assert property (stability);
+    assert property (no_glitch);
 endmodule: dbl_sync_asserts
+
+// NOTE: for more cdc assertions,
+// https://www.techdesignforums.com/practice/technique/multiple-cross-clock-domain-verifcation/
